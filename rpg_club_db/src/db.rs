914d2786c34f2003ca075e247a87aaf0953da609
Matthieu Lux <matthieu.lux@protonmail.com>
2024-12-24 14:33:59 +0100

    update docker rust version and configure db path with an env variable

--- rpg_club_db/src/db.rs
+++ rpg_club_db/src/db.rs
@@ -1,19 +1,28 @@
+use std::env;
 use std::fs;
 
 use rusqlite::{Connection, Result};
 
-const DB_PATH: &str = "../rpg_club_db/db/rpg_club.db";
-const INIT_SCRIPT_PATH: &str = "../rpg_club_db/db/init.sql";
+const DEFAULT_PATH: &str = "../rpg_club_db/db/rpg_club.db";
+const DEFAULT_INIT_SCRIPT_PATH: &str = "../rpg_club_db/db/init.sql";
+
+fn get_db_path() -> String {
+    env::var("DB_PATH").unwrap_or_else(|_| DEFAULT_PATH.to_string())
+}
+
+fn get_init_script_path() -> String {
+    env::var("DB_INIT_SCRIPT_PATH").unwrap_or_else(|_| DEFAULT_INIT_SCRIPT_PATH.to_string())
+}
 
 pub fn new() -> Result<Connection> {
-    Connection::open(DB_PATH)
+    Connection::open(get_db_path())
 }
 
 pub fn init() -> Result<()> {
     let conn = new()?;
 
-    // Read and execute initialization SQL
-    let init_sql = fs::read_to_string(INIT_SCRIPT_PATH).expect("Failed to read init.sql file");
+    let init_sql =
+        fs::read_to_string(get_init_script_path()).expect("Failed to read init.sql file");
     conn.execute_batch(&init_sql)?;
 
     println!("Database initialized successfully!");
