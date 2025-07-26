27f17061c802b534fc2ce79c09efcb3cf15bf300
Matthieu Lux <matthieu.lux@protonmail.com>
2025-07-26 16:55:29 +0200

    update game view

---
+++ src/lib/components/game-card.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import SupabaseImage from '$lib/components/image/supabase-image.svelte';
+	import type { Game } from '$lib/types';
+	import SvelteMarkdown from 'svelte-markdown';
+	import Pen from '@lucide/svelte/icons/pen';
+	import ChevronDown from '@lucide/svelte/icons/chevron-down';
+	import ChevronUp from '@lucide/svelte/icons/chevron-up';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+
+	type Props = {
+		game: Game;
+		supabase: SupabaseClient;
+	};
+
+	const { game, supabase }: Props = $props();
+	let showDetails = $state(false);
+
+	const handleClick = (game: Game | undefined) => () => {
+		goto(`/games/${game?.id ? game.id : 'new'}/edit`);
+	};
+</script>
+
+<div
+	class="card preset-filled-surface-100-900 border-surface-200-800 card-hover divide-surface-200-800 block w-full divide-y overflow-hidden rounded-xl border"
+>
+	<!-- Header -->
+	<header class="relative">
+		<SupabaseImage
+			{supabase}
+			bucket="game-banners"
+			url={game.illustration}
+			alt={game.name}
+			className="aspect-[21/9] w-full object-cover"
+		/>
+		<button
+			class="btn-icon preset-tonal-primary absolute top-4 right-4"
+			onclick={handleClick(game)}
+		>
+			<Pen size={16} />
+		</button>
+		<button
+			class="btn preset-tonal-primary absolute bottom-2 left-1/2 -translate-x-1/2 transform"
+			onclick={() => (showDetails = !showDetails)}
+		>
+			{#if showDetails}
+				Less <ChevronUp size={16} />
+			{:else}
+				More <ChevronDown size={16} />
+			{/if}
+		</button>
+	</header>
+	<!-- Article -->
+	<article class={`space-y-4 p-4 ${showDetails ? 'block' : 'hidden'}`}>
+		<div>
+			<h3 class="h3">{game.name}</h3>
+		</div>
+		<p class="prose prose-invert prose-sm opacity-60">
+			<SvelteMarkdown source={game.description || ''} />
+		</p>
+	</article>
+</div>
--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,49 +1,18 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import SupabaseImage from '$lib/components/image/supabase-image.svelte';
-	import type { Game } from '$lib/types';
-	import SvelteMarkdown from 'svelte-markdown';
+	import GameCard from '$lib/components/game-card.svelte';
 
 	const { data } = $props();
 	const { supabase, games } = data;
-
-	const handleClick = (game: Game | undefined) => () => {
-		goto(`/games/${game?.id ? game.id : 'new'}/edit`);
-	};
 </script>
 
-<div class="table-wrap mx-auto max-w-4xl">
-	<table class="table caption-bottom">
-		<thead>
-			<tr>
-				<th>Nom</th>
-				<th>Description</th>
-				<th>Illustration</th>
-			</tr>
-		</thead>
-		<tbody class="[&>tr]:hover:preset-tonal-primary">
-			{#if games && games.length > 0}
-				{#each games as game (game.id)}
-					<tr onclick={handleClick(game)} class="cursor-pointer">
-						<td>{game.name}</td>
-						<td class="prose prose-invert">
-							<SvelteMarkdown source={game.description || ''} />
-						</td>
-						<td class="w-sm">
-							<SupabaseImage
-								{supabase}
-								bucket="game-banners"
-								url={game.illustration}
-								alt={game.name}
-								className="h-48 w-sm rounded-2xl object-cover"
-							/>
-						</td>
-					</tr>
-				{/each}
-			{/if}
-			<tr onclick={handleClick(undefined)} class="cursor-pointer text-center">
-				<td colspan="100">+</td>
-			</tr>
-		</tbody>
-	</table>
+<div class="mx-auto flex w-4/5 flex-col items-center gap-8 pb-20 md:w-3xl">
+	<div class="flex w-full justify-end">
+		<button class="btn preset-filled-primary-500">Add +</button>
+	</div>
+
+	{#if games && games.length > 0}
+		{#each games as game (game.id)}
+			<GameCard {game} {supabase} />
+		{/each}
+	{/if}
 </div>
